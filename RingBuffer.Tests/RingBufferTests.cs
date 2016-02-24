00cc1b7afb1b84d82c3a6b95e40562e51d7a3f75
luke <luke@ais.pl>
2016-02-24 18:53:46 +0100

    Ring Buffer Initial Commit

---
+++ RingBuffer.Tests/RingBufferTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Xunit;
+
+namespace RingBuffer.Tests
+{
+    public class RingBufferTests
+    {
+        [Fact]
+        public void CorrectAddElementAndRemoveWhenNotExeeded()
+        {
+            var buffer = new RingBuffer<int>(3);
+            buffer.Enqueue(10);
+            buffer.Enqueue(11);
+
+            Assert.Equal(10, buffer.Dequeue());
+            Assert.Equal(11, buffer.Dequeue());
+        }
+
+        [Fact]
+        public void OverIncrementStartPointerShouldOverrightFirstValue()
+        {
+            var buffer = new RingBuffer<int>(3);
+            buffer.Enqueue(10);
+            buffer.Enqueue(11);
+            buffer.Enqueue(12);
+            buffer.Enqueue(14);
+
+            var element = buffer.Dequeue();
+
+            Assert.Equal(element,12);
+        }
+
+        [Fact]
+        public void GettingElementsFromEmptyCollectionShouldThrowsInvalidOperationException()
+        {
+            var buffer = new RingBuffer<int>(3);
+
+            Assert.Throws<InvalidOperationException>(() => buffer.Dequeue());
+        }
+
+
+        [Fact]
+        public void AddingMultipleElementsShouldNotRisedArgumentOutOfRangeException()
+        {
+            var buffer = new RingBuffer<int>(3);
+            
+            buffer.Enqueue(1);
+            buffer.Enqueue(2);
+            buffer.Enqueue(3);
+
+            var ex = Record.Exception(() => buffer.Enqueue(4));
+
+            Assert.Null(ex);
+        }
+
+        [Fact]
+        public void PoliteReciveLastElementShouldNotRemoveItFromQueueAndMoveReadPointer()
+        {
+            var buffer = new RingBuffer<int>(3);
+            
+            buffer.Enqueue(1);
+            buffer.Enqueue(2);
+
+            var firstElement = buffer.LastElement;
+            var secondElement = buffer.LastElement;
+
+            Assert.Equal(firstElement, secondElement);
+        }
+
+        [Fact]
+        public void LastElementExpressionBodyShouldThowExceptionWhenCollectionIsEmpty()
+        {
+            var buffer = new RingBuffer<int>(2);
+
+            Assert.Throws<InvalidOperationException>(() => buffer.LastElement);
+        }
+
+        [Fact]
+        public void WhenRingBufferHasBeenClearedIsEmptyFlagShouldBeSetToTrue()
+        {
+            var buffer = new RingBuffer<int>(2);
+            buffer.Enqueue(2);
+            buffer.Enqueue(1);
+
+            buffer.Clear();
+
+            Assert.Equal(buffer.IsEmpty, true);
+        }
+    }
+}
---
+++ RingBuffer/RingBuffer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Policy;
+
+namespace RingBuffer
+{
+    public class RingBuffer<T>
+    {
+        private T[] _table;
+
+        private int _readPointer;
+
+        private int _writePointer;
+
+        private int _capacity;
+
+        private int _count;
+
+        public T LastElement
+        {
+            get
+            {
+                if (_count == 0)
+                {
+                    throw new InvalidOperationException("Collection is empty.");
+                }
+
+                return _table[_readPointer];
+            }
+        }
+
+        public bool IsEmpty => _count == 0;
+
+        public RingBuffer(int capacity)
+        {
+            _capacity = capacity;
+            _table = new T[_capacity];
+            _readPointer = 0;
+            _writePointer = 0;
+        }
+
+        public void Enqueue(T value)
+        {
+            _table[_writePointer] = value;
+            _writePointer = ++ _writePointer % _capacity;
+            _count++;
+            if (_writePointer == _readPointer)
+            {
+                _readPointer = ++_readPointer%_capacity;
+            }
+        }
+
+        public T Dequeue()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("Collection is empty.");
+            }
+
+            _count--;
+            var element = _table[_readPointer];
+            _readPointer = ++_readPointer%_capacity;
+
+            return element;
+        }
+
+        public void Clear()
+        {
+            _writePointer = 0;
+            _readPointer = 0;
+            _count = 0;
+            for (var i = 0; i < _table.Length; i++)
+            {
+                _table[i] = default(T);
+            }
+        }
+    }
+}
